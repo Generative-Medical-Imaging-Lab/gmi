70308a33f1ba274aec095136ed4e18599773618b
Matthew Tivnan <tivnanmatt@gmail.com>
2024-10-31 09:41:55 -0400

    added __init__.py to gmi package

---
+++ gmi/__init__.py
@@ -0,0 +1,10 @@
+# /gmi/gmi/__init__.py: Initializes the gmi package
+
+from . import diffusion
+from . import distributions
+from . import linalg
+from . import networks
+from . import samplers
+from . import datasets
+from . import sde
+from . import tasks
